2152b35b68cdc5f883876d4a810cef4c5f7f2261
JianingFuuuuu <138001987+JianingFuuuuu@users.noreply.github.com>
2026-03-30 14:19:52 -0500

    display winner when three squares match

--- src/App.jsx
+++ src/App.jsx
@@ -25,7 +25,14 @@ function Board({ xIsNext, squares, onPlay }) {
     onPlay(nextSquares);
   }
 
-  const status = 'Next player: ' + (xIsNext ? 'X' : 'O');
+  const winner = calculateWinner(squares);
+  let status;
+
+  if (winner) {
+  status = 'Winner: ' + winner;
+    } else {
+  status = 'Next player: ' + (xIsNext ? 'X' : 'O');
+}
 
   return (
     <>
