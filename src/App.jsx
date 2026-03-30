726f00da2be5b2254379649b021c2c32105ca6ca
JianingFuuuuu <138001987+JianingFuuuuu@users.noreply.github.com>
2026-03-30 14:15:33 -0500

    render move history list from game history

--- src/App.jsx
+++ src/App.jsx
@@ -59,7 +59,16 @@ export default function Game() {
     setXIsNext(!xIsNext);
   }
 
-  const moves = [<li key={0}>Go to game start</li>];
+  const moves = history.map((squares, move) => {
+    let description;
+    if (move > 0) {
+      description = 'Go to move #' + move;
+    } else {
+      description = 'Go to game start';
+    }
+
+    return <li key={move}>{description}</li>;
+  });
 
   return (
     <div className="game">
